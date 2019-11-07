3de117950462460e427a249935261dbfeedb877b
dereklowlind <dereklowlind@gmail.com>
2019-11-07 08:17:18 -0800

    Updates

---
+++ precache-manifest.427f8e834968e9ce09564c444f6215c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb7e4d81a10b03c1e0f6e1ca2cb753a",
+    "url": "/lecshare-main/index.html"
+  },
+  {
+    "revision": "a395d94ed9e424c2c6b4",
+    "url": "/lecshare-main/static/css/main.5865ad94.chunk.css"
+  },
+  {
+    "revision": "ee46aaa40bb2a5035281",
+    "url": "/lecshare-main/static/js/2.72e6c971.chunk.js"
+  },
+  {
+    "revision": "a395d94ed9e424c2c6b4",
+    "url": "/lecshare-main/static/js/main.3e552efe.chunk.js"
+  },
+  {
+    "revision": "6c1b9608a5553909e25c",
+    "url": "/lecshare-main/static/js/runtime-main.2f8d58cf.js"
+  },
+  {
+    "revision": "61347656c37c3810cf1fc7f94973cc35",
+    "url": "/lecshare-main/static/media/protoslideimage.61347656.jpg"
+  }
+]);
